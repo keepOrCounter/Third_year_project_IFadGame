2f1b8ed8ad554904635dae135480a7f6f1dcefd3
Wai Hou Leong <wai.leong@student.manchester.ac.uk>
2024-02-03 09:33:25 +0000

    Input checker version alpha 0.02

--- modules/interactionSys.py
+++ modules/interactionSys.py
@@ -5,7 +5,8 @@ import Levenshtein
 from status_record import *
 from Pre_definedContent import *
 import json
-from autospell import Speller
+from autocorrect import Speller
+from numpy import inf
 
 class IOSys():
     def __init__(self) -> None:
@@ -55,14 +56,12 @@ class OutputGenerator():
 player is at for a text-based adventure game program, you will receive a game details from game \
 program like this "{Current location: End Of Road, Front: brick building, Back: Forest, \
 Right hand side: Forest, Left hand side: Forest, Landscape Features: [flat ground, small stream], \
-Items: [keyA, keyB, keyC]}". You should write a description in this style: "
-
+Items: [keyA, keyB, keyC]}". Here is the example of expected result: "
 End Of Road
-
 You are standing at the end of a road before a small brick building. Around you is a forest. A small stream flows out of the building and down a gully. 
 There are some keys on the ground here." """
 
-        self.__eventDiscriptionSysRole = """You are creating a event for a text-based adventure game, you should create event in following form based on game information provided in later:
+        self.__eventDescriptionSysRole = """You are creating a event for a text-based adventure game, you should create event in following form based on game information(Mainly the triggered reason) provided in later:
 {
 	"event_name": <event name here>,
 	"event_discription": <possible discription in third person and first person view>
@@ -77,7 +76,6 @@ There are some keys on the ground here." """
 		"player current status": "Normal",
 		"description needed to be modified": "
 End Of Road
-
 You are standing at the end of a road before a small brick building. Around you is a forest. A small stream flows out of the building and down a gully. 
 There are some keys on the ground here."
 	}
@@ -85,12 +83,37 @@ There are some keys on the ground here."
 	expected result:
 	{
 		"event_name": "starting feeling tired",
-		"event_discription": "
+		"event_description": "
 End Of Road
-
 You stand at the end of a road before a small brick building. The dense forest surrounds you, its looming trees casting shadows. A weary sensation seeps through your limbs, accentuating the fatigue in your bones. A small stream trickles from the building, and amidst the weariness, you notice a glint—keys scattered on the ground, waiting to be claimed."
 	}"""
         
+        self.__eventDevelopmentSysRole = """You are determine the development of an event in a text-based adventure game, you should tell the program the development in following form of event based on (Mainly based on the triggered reason and player information) the game information provided later:
+{
+	"successful": <True if succeed, False if not>,
+	"fail": <True if failed, False if not>,
+	"reward": <select the indices of zero or more of reward like a python list (You may make this with an empty list if you decide to choose no reward) from possible reward list in game information provided based on player action in game information>, 
+	"penalty": <select the indices of zero or more of penalty like a python list (You may make this with an empty list if you decide to choose no penalty) from possible penalty list in game information provided based on player action in game information>
+}, Here is an example(the event is not necessary be succeed or failed immediately, it won't failed as long as in the time limit, we will tell you if times up), the following example has select a penalty which is the third one in "possible penalty":
+	game information:
+	{
+		"event_name": "Venomous Encounter",
+		"event type": "poisoning potential",
+		"triggered reason": "attacked by snake",
+		"player current status": "normal",
+		"player action": "suck the attcked part",
+		"times up": False,
+		"possible reward": ["increase maximum hp", "increase maximum action point", "obtain a poison"],
+		"possible penalty": ["decrease hp", "decrease action point", "add poisoning status"]
+	}
+	
+	expected result:
+	{
+		"successful": "False",
+		"fail": "False",
+		"reward": [], 
+		"penalty": [2]
+}"""
     
     def locationDescription(self, locationList: dict[str, Location]) -> None:
         """
@@ -115,10 +138,14 @@ You stand at the end of a road before a small brick building. The dense forest s
         gpt_response = self.__gptAPI.inquiry(inquiry, self.__locationDiscriptionSysRole)
         # print(gpt_response)
         self.__OuterData.inquery_response_log_recorder(self.__locationDiscriptionSysRole, inquiry, gpt_response)
+        
+        # result = json.loads(gpt_response, strict=False)
+        
         locationList["Current location"].description = gpt_response
     
     
-    def eventDescription(self, event: Events) -> dict[str]:
+    def eventDescription(self, event: Events) -> None:
+        # TODO change the eventDescription to make it description all current events
         inputDictionary = {"event type": event.eventType, "triggered reason": event.triggered_reason, \
             "Current location": event.current_location, "Current action": event.currentAction, \
                 "Tool(s) assist with moving": event.moving_tool, "player current status": event.play_current_status, \
@@ -127,17 +154,37 @@ You stand at the end of a road before a small brick building. The dense forest s
         inquiry = str(inputDictionary)
         print(inquiry)
         print("=======================================\n")
-        gpt_response = self.__gptAPI.inquiry(inquiry, self.__eventDiscriptionSysRole)
+        gpt_response = self.__gptAPI.inquiry(inquiry, self.__eventDescriptionSysRole)
         # print(gpt_response)
-        self.__OuterData.inquery_response_log_recorder(self.__eventDiscriptionSysRole, inquiry, gpt_response)
+        self.__OuterData.inquery_response_log_recorder(self.__eventDescriptionSysRole, inquiry, gpt_response)
         
-        data = json.loads(gpt_response, strict=False)
+        result = json.loads(gpt_response, strict=False)
 
         # Extract the value of the "event_name" key and "event_description" key
-        result = {"event_name": str(data.get("event_name")), "event_description": str(data.get("event_description"))}
+        # result = {"event_name": str(data.get("event_name")), "event_description": str(data.get("event_description"))}
+        event.eventName = result["event_name"]
+        event.description = result["event_description"]
 
+        # return result
+        
+    def eventDevelopment(self, event: Events) -> None:
+        # TODO
+        inputDictionary = {"event_name": event.eventName, "event type": event.eventType, \
+            "triggered reason": event.triggered_reason, "player current status": event.play_current_status,\
+                "player action": event.currentAction, "times up": event.triggered_time > event.time_limit, \
+                    "possible reward": event.possible_reward, "possible penalty": event.possible_penalty
+                }
+        
+        inquiry = str(inputDictionary)
+        print(inquiry)
+        print("=======================================\n")
+        gpt_response = self.__gptAPI.inquiry(inquiry, self.__eventDevelopmentSysRole)
+        # print(gpt_response)
+        self.__OuterData.inquery_response_log_recorder(self.__eventDevelopmentSysRole, inquiry, gpt_response)
+        
+        result = json.loads(gpt_response, strict=False)
 
-        return result
+        print(result)
     
 class InputTranslator():
     def __init__(self, gptAPI: Gpt3, playerStatus: Player_status, mapInfo: Map_information, \
@@ -146,6 +193,26 @@ class InputTranslator():
         self.__playerStatus = playerStatus
         self.__mapInfo = mapInfo
         self.__defined_content = defined_content
+
+    def frequent_command (common_list:str) -> clear_word_list:
+        frequent_words_in_commands = []
+        common_list.pop(-1)
+
+        for i in range(len(common_list)):
+            common_words = common_list[i].split()
+            for i in range(len(common_words)):
+                frequent_words_in_commands.append(common_words[i])
+
+        clear_word_list = list(dict.fromkeys(frequent_words_in_commands))
+
+    def all_case_word_list (word_list:str) -> all_case_word_list:
+        all_case_word_list = []
+
+        for i in range(len(word_list)):
+            all_case_word_list.append(word_list[i].lower())
+
+        for i in range(len(word_list)):
+            all_case_word_list.append(word_list[i].upper())
         
     def command_translator(self, user_input:str):
         move_commands = list(self.__defined_content.get_Actions().keys())
@@ -156,9 +223,16 @@ system, the game command are listed below: " + str(move_commands[:-1]) + "\nPlea
 not reply something more than the command given above(Even if punctuation mark). If the player command is less likely to \
 be any of the game command above, just reply a '<Rejected>'."
 
-        spell = Speller(lang = 'en')
-        corrected_user_input = spell(user_input)
+        clear_word_list_in_move_commands = InputTranslator.frequent_command(move_commands)
+        all_case_word_list = InputTranslator.all_case_word_list(clear_word_list_in_move_commands)
         
+        spell = Speller(lang = 'en')
+
+        for i in range(len(all_case_word_list)):
+            spell.nlp_data.update({all_case_word_list[i]:inf})
+
+        corrected_user_input = spell(user_input)
+
         if (corrected_user_input != user_input):
             print("Wait, why I have that mind, is it\"", corrected_user_input, "\"?")
         else:
