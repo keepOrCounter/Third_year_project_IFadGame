fcaeab2d4a126d0579e1fd447b9754ab107ed38c
WaiHL <wai.leong@student.manchester.ac.uk>
2024-04-14 21:52:51 +0100

    Updated test and change the format

--- test/wordnetTest.py
+++ test/wordnetTest.py
@@ -53,8 +53,7 @@
 #     for synset in synsets:
 #         for lemma in synset.lemmas():
 #             synonymsList.append(lemma.name())
-#     print(f"\nSynonyms for '{word}':")
-#     synonymsList = list(dict.fromkeys(synonymsList))
+#     print(f"\nSynonyms for '{word}':")#     synonymsList = list(dict.fromkeys(synonymsList))
 #     print(synonymsList)
  
 #     # 显示短语中其他单词的同义词集合
@@ -65,21 +64,83 @@
 # else:
 #     print(f"No meanings found for '{word}' in the context of the phrase")
 
-import nltk
-import sys
-from os.path import expanduser
-home = expanduser("~")
-from nltk.tag.hunpos import HunposTagger
-_path_to_bin = home + '\\hunpos-1.0-win\\hunpos-tag.exe'
-_path_to_model = home + '\\hunpos-1.0-win\\english.model'
-ht = HunposTagger(path_to_model=_path_to_model, path_to_bin=_path_to_bin)
-text = "go north"
-def verbFinder(text):
-    tagged_tokens = ht.tag(text.split())
-    print(tagged_tokens)
-    for token, pos in tagged_tokens:
-        pos = str(pos, 'utf-8')
-        if pos[0].lower() == 'v':
-            return token
-print(verbFinder(text))
-ht.close()
+# import nltk
+# import sys
+# from os.path import expanduser
+# home = expanduser("~")
+# from nltk.tag.hunpos import HunposTagger
+# _path_to_bin = home + '\\hunpos-1.0-win\\hunpos-tag.exe'
+# _path_to_model = home + '\\hunpos-1.0-win\\english.model'
+# ht = HunposTagger(path_to_model=_path_to_model, path_to_bin=_path_to_bin)
+# text = "aloe vera"
+# def verbFinder(text):
+#     tagged_tokens = ht.tag(text.split())
+#     print(tagged_tokens)
+#     for token, pos in tagged_tokens:
+#         pos = str(pos, 'utf-8')
+#         if pos[0].lower() == 'v':
+#             return token
+# print(verbFinder(text))
+# ht.close()
+
+
+################################################################
+from spellchecker import SpellChecker
+
+lst = input()
+
+split_lst = lst.split()
+
+spell = SpellChecker()
+
+for i in range(len(split_lst)):
+    if split_lst[i] == "aloe" or split_lst[i] == "vera" or split_lst[i] == "unequip":
+        pass
+    else:
+        misspelled = spell.unknown([split_lst[i]])
+        for word in misspelled:
+            checked_word = spell.correction(word)
+            if split_lst[i] != checked_word:
+                split_lst[i] = checked_word
+
+print(' '.join(split_lst))
+
+# spell = SpellChecker()
+
+# # find those words that may be misspelled
+# misspelled = spell.unknown(["havr"])
+# print(misspelled)
+
+# for word in misspelled:
+#     # Get the one `most likely` answer
+#     print(spell.correction(word))
+
+#     print(spell.candidates(word))
+
+
+###############################################################
+
+
+
+
+# import jamspell
+
+# corrector = jamspell.TSpellCorrector()
+# corrector.LoadLangModel('en.bin')
+
+# corrector.FixFragment('I am the begt spell cherken!')
+# # u'I am the best spell checker!'
+
+
+# from spylls.hunspell import Dictionary
+
+# # en_US dictionary is distributed with spylls
+# # See docs to load other dictionaries
+# dictionary = Dictionary.from_files('en_US')
+
+# print(dictionary.lookup('aloe vera'))
+# # False
+# for suggestion in dictionary.suggest('aloe vera'):
+#     print(suggestion)
+# spells
+# spills
